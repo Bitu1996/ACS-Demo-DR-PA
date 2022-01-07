5ad77f92a20bc86c743fe5eb11be94daf1020bdf
SHANTI LAL <shandude786@users.noreply.github.com>
2022-01-07 14:13:11 +0530

    first commit

---
+++ src/config.js
@@ -0,0 +1,11 @@
+export const constants = {
+  KEY_AUTH_TOKEN: "AUTH_KEY",
+  KEY_USER_TYPE: "USER_TYPE",
+  KEY_USER_NAME: "USER_NAME",
+  KEY_DISPLAY_NAME: "DISPLAY_NAME",
+  KEY_SPOOL_ID: "SPOOL_ID",
+  KEY_SPOOL_TOKEN: "SPOOL_TOKEN",
+
+  endpoint: "https://acs-med-app-api.azurewebsites.net",
+  acsEndpoint: "https://acs-med-app.communication.azure.com/",
+};
